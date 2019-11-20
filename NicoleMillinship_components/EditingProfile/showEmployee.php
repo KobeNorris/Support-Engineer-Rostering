33618669414d0a86c431b1e4faf27b59a149f9a0
psynm6 <psynm6@nottingham.ac.uk>
2019-11-20 13:41:17 +0000

    holidays and deployments are shown

--- NicoleMillinship_components/EditingProfile/showEmployee.php
+++ NicoleMillinship_components/EditingProfile/showEmployee.php
@@ -2,6 +2,9 @@
 require_once('connectionTest.php');
 $query = "SELECT * FROM employee"; 
 $employeeRecord = mysqli_query($connect, $query);
+
+$query2 = "SELECT * FROM holiday WHERE HolidayStartDate > CURDATE()";
+$query3 = "SELECT * FROM deployment WHERE DeploymentStartDate > CURDATE()";
 ?>
 
 <!DOCTYPE html>
@@ -12,52 +15,92 @@ $employeeRecord = mysqli_query($connect, $query);
     </head>
     <body>
         
-        <?php while ($row = mysqli_fetch_array($employeeRecord)) { ?>
+        <?php while ($eRow = mysqli_fetch_array($employeeRecord)) { ?>
             <div>
-                <img id="profilePicture" src="uploads/defaultImage.png">
+                <?php $profilePics = "uploads/";
+                $profilePics .= $eRow['ProfilePicture'];
+                echo "<img id=profilePic src=$profilePics>"; ?> 
             </div>
             <p>
                 Employee Name:
-                <?php echo $row['FirstName'];?> 
-                <?php echo $row['Surname'];?>
+                <?php echo $eRow['FirstName'];?> 
+                <?php echo $eRow['Surname'];?>
             </p>
             
             <p>
                 Work ID:
-                <?php echo $row['WorkID'];?> 
+                <?php echo $eRow['WorkID'];?> 
             </p>
 
             <p>
                 Email Address:
-                <?php echo $row['EmailAddress'];?> 
+                <?php echo $eRow['EmailAddress'];?> 
             </p>
 
             <p>
                 Telephone Number:
-                <?php echo $row['TelephoneNumber'];?> 
+                <?php echo $eRow['TelephoneNumber'];?> 
             </p>
 
             <p>
                 Current Job Role:
-                <?php echo $row['CurrentJobRole'];?> 
+                <?php echo $eRow['CurrentJobRole'];?> 
             </p>
 
             <p>
                 Slack ID: 
-                <?php echo $row['SlackID'];?> 
+                <?php echo $eRow['SlackID'];?> 
             </p>
 
             <p>
                 Status: 
-                <?php if ($row['SlackID'] == 0)
+                <?php if ($eRow['Status'] == 0)
                     echo "Inactive";
                 else
                     echo "Active";
                 ?> 
             </p>
 
-            <p>[List of future holidays here]</p>
-            <p>[List of future deployments here]</p>
+            <p>
+                Future Holidays:
+                <table align="center">
+                <tr> 
+                    <th> Start Date </th>
+                    <th> End Date </th>
+                </tr>
+                <?php $holidays = mysqli_query($connect, $query2);
+                while ($hRow = mysqli_fetch_array($holidays)) {
+                    if ($eRow['WorkID'] == $hRow['WorkID']) {
+                        echo "<tr>";
+                        echo "<td>" . $hRow['HolidayStartDate'] . "</td>";
+                        echo "<td>" . $hRow['HolidayEndDate'] . "</td>"; 
+                        echo "</tr>";
+                    }
+                }
+                ?>
+                </table>
+            </p>
+
+            <p>
+                Future Deployments:
+                <table align="center">
+                    <tr> 
+                        <th> Start Date </th>
+                        <th> End Date </th>
+                    </tr>
+                    <?php $deployments = mysqli_query($connect, $query3);
+                    while ($hRow = mysqli_fetch_array($deployments)) {
+                        if ($eRow['WorkID'] == $hRow['WorkID']) {
+                            $isEmpty = false;
+                            echo "<tr>";
+                            echo "<td>" . $hRow['DeploymentStartDate'] . "</td>";
+                            echo "<td>" . $hRow['DeploymentEndDate'] . "</td>"; 
+                            echo "</tr>";
+                        }
+                    }
+                    ?>
+                    </table>
+            </p>
 
         <?php }; ?>
 
