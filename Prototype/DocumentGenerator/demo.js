c875177c244fb75d7bda7da49fe23ed35a68504e
KobeNorrise <Kobewu522@163.com>
2020-02-18 19:13:06 +0000

    Update the file generator

--- Prototype/DocumentGenerator/demo.js
+++ Prototype/DocumentGenerator/demo.js
@@ -1,15 +1,29 @@
 function generateDocument() {
-    // var file = new File(["Hello, world!"], "hello world.txt", { type: "text/plain;charset=utf-8" });
 
-    // saveAs(file);
+    // var EmployeeProfile = new Blob(["Welcome to Websparrow.org."], { type: "txt/plain;charset=utf-8" });
 
-    // var blob = new Blob(["Hello, world!"], { type: "text/plain;charset=utf-8" });
+    // saveAs(EmployeeProfile, "static.txt");
 
-    // saveAs(blob, "hello world.txt");
+    var wb = XLSX.utils.book_new();
+    wb.Props = {
+        Title: "Test excel",
+        Subject: "Test",
+        Author: "Kejia Wu",
+        CreatedDate: new Date()
+    };
+    wb.SheetNames.push("Test Sheet");
+    var ws_data = [['hello', 'world']];
+    var ws = XLSX.utils.aoa_to_sheet(ws_data);
+    wb.Sheets["Test Sheet"] = ws
 
-    var EmployeeProfile = new Blob(["Welcome to Websparrow.org."], { type: "txt/plain;charset=utf-8" });
+    var wbout = XLSX.write(wb, { bookType: 'xlsx', type: 'binary' });
 
-    // console.log(EmployeeProfile);
+    saveAs(new Blob([s2ab(wbout)], { type: "application/octet-stream" }), 'test.xlsx');
+}
 
-    saveAs(EmployeeProfile, "static.txt");
+function s2ab(s) {
+    var buf = new ArrayBuffer(s.length); //convert s to arrayBuffer
+    var view = new Uint8Array(buf);  //create uint8array as viewer
+    for (var i = 0; i < s.length; i++) view[i] = s.charCodeAt(i) & 0xFF; //convert to octet
+    return buf;
 }
